0cd76ec5a8918c837173d5519bf1d8744d6a79cc
Victor Toschi <victor.toschi@hotmail.com>
2016-07-01 18:10:14 -0300

    Mudança de estrutura

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,43 @@
 'use strict';
 
-var gulp       = require('gulp'),
-	connect    = require('gulp-connect'),
-	pkg        = require('./package.json');
+const gulp       = require('gulp'),
+	  connect    = require('gulp-connect'),
+	  pkg        = require('./package.json'),
+	  sass       = require('gulp-sass');
 
-gulp.task('connect', function(){
+gulp.task('connect', function() {
 	return connect.server({
 		root: './source/',
 		livereload: true
 	});
 });
 
-gulp.task('html', function(){
+gulp.task('html', function() {
 	gulp.src('./source/index.html')
     	.pipe(connect.reload());
 });
 
-gulp.task('css', function(){
+/*gulp.task('css', function(){
 	gulp.src('./source/css/style.css')
+    	.pipe(connect.reload());
+});*/
+
+gulp.task('sass', function () {
+	return gulp.src('./source/sass/*.scss')
+    	.pipe(sass().on('error', sass.logError))
+    	.pipe(gulp.dest('./source/css'))
     	.pipe(connect.reload());
 });
 
-gulp.task('js', function(){
+gulp.task('js', function() {
 	gulp.src('./source/js/*')
     	.pipe(connect.reload());
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', function() {
 	gulp.watch('./source/index.html', ['html']);
-	gulp.watch('./source/css/style.css', ['css']);
+	gulp.watch('./source/sass/*.scss', ['sass']);
+	//gulp.watch('./source/css/style.css', ['css']);
 	gulp.watch('./source/js/*', ['js']);
 });
 
--- source/js/apps.js
+++ source/js/apps.js
@@ -1,27 +1,7 @@
 (function($) {
-	$('#presentation').fadeIn(2000, function() {
-		$('#about-me').slideDown('fast', function() {
-			$('#work').fadeIn('slow');
-		});
-	});
 
-	$('#about-me').find('a').hover(
-		function() {
-			$('.icon-about').fadeIn('slow');
-		}, function() {
-			$('.icon-about').fadeOut('fast');
-		}
-	);
+	var home  = $('#home'),
+		about = $('#about'),
+		works = $('#works');
 
-	$('#work').find('a').hover(
-		function() {
-			$('.icon-work').fadeIn('slow');
-		}, function() {
-			$('.icon-work').fadeOut('fast');
-		}
-	);
-
-	$('#about-me').on('mousedown', function() {
-		$('#home').toggle( "slide" );
-	});
 })(jQuery);
